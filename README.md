93607abc1153fe45848d7c92b4c96cf866641465
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-05-15 18:59:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kotoba-player-py
+ 自然言語処理で遊ぶライブラリ
---
+++ kotoba_player_py/api.py
@@ -0,0 +1,9 @@
+class KotobaPlayer:
+    def __init__(self) -> None:
+        pass
+
+    def parrot(self, text:str) -> str:
+        pass
+
+    def masquerade(self, text:str, mask_token:str, mask_char_by_char:bool=False) -> str:
+        pass
